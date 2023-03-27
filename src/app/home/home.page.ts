05b0b8afda2e116e33225c3394dedddc4f2389ad
DiegoCP17 <diego.cordoba@correo.tdea.edu.co>
2023-03-27 01:35:34 -0500

    Interfaz Home finalizada & nuevos archivos agregados

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // packages = [
+  //   {
+  //     place: 'Apartamentos J.O',
+  //     location: 'Antioquia',
+  //     bg:'/src/assets/Imagenes/pintor.jpg'
+      
+  //   },
+  //   {
+  //     place: 'Edificio los olivos',
+  //     location: 'Cali',
+  //     bg:'/src/assets/Imagenes/Electricista.jpg'
+      
+  //   },
+  //   {
+  //     place: 'Aguas Claras',
+  //     location: 'Santa Marta',
+  //     bg:'/src/assets/Imagenes/plomero.jpg'
+      
+  //   }
+  // ]
 
+  Salir() {
+    
+    alert("Ha Cerrado Sessión");
+  }
   constructor() {}
-
+  
 }
