77192760545b9133340c5b85f44828a6e73d7551
DiegoCP17 <diego.cordoba@correo.tdea.edu.co>
2023-03-27 23:48:46 -0500

    Conecxion a BD & Correccion de Errores

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { LoadingController, NavController, ToastController } from '@ionic/angular';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-register',
-  templateUrl: './register.page.html',
+  templateUrl: '../register/register.page.html',
   styleUrls: ['../login/login.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  
+  User = {} as User;
+  
 
-  constructor() { }
+  constructor(
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private afAuth: AngularFireAuth,
+    private navCtrl: NavController
+  ) { }
 
-  ngOnInit() {
+  
+
+  ngOnInit() {}
+
+  async register(user: User) {
+  if (this.formValidation(user)) { // Correctly pass the user object to formValidation()
+    let loader = await this.loadingCtrl.create({
+      message: "Espere por favor...."
+    });
+    await loader.present();
+
+    try {
+      const data = await this.afAuth.createUserWithEmailAndPassword(user.email, user.password); // Use await instead of .then()
+      console.log(data);
+
+      this.navCtrl.navigateRoot("home");
+    } catch (error:any) {
+      error.message = "Error al Registrarse!";
+      let errorMessage = error.message || error.getLocalizeMessage();
+
+      this.showToast(errorMessage);
+    }
+
+    await loader.dismiss();
+
   }
+}
+formValidation(user: User) { // Correct the function name and accept the user object as a parameter
+  if (!user.email) { // Use the user object to validate email and password
+    this.showToast("Ingrese un Email");
+    return false;
+  }
+  if (!user.password) {
+    this.showToast("Ingrese una Contraseña");
+    return false;
+  }
+  return true; // Return true here as validation passed
+}
 
+showToast(message: string) { // Pass the message parameter to showToast function
+  this.toastCtrl.create({
+    message: message, // Use the message parameter
+    duration: 4000
+  }).then(toastData => toastData.present());
+}
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  FIREBASE_CONFIG:{
+    apiKey: "AIzaSyB1tQ0bScDyvagUooF9p_v540cVBoUi6R0",
+    authDomain: "appionictdea-7f53f.firebaseapp.com",
+    projectId: "appionictdea-7f53f",
+    storageBucket: "appionictdea-7f53f.appspot.com",
+    messagingSenderId: "465037763651",
+    appId: "1:465037763651:web:7588840b41fe64f575e0f6",
+    measurementId: "G-T345Z41EDM"
+  } // CONSTANTE DE CONFIGURACION CON FIREBASE
+};  
 
 /*
  * For easier debugging in development mode, you can import the following file
