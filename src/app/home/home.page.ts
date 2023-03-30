48f5f00b8d3b260fa331cdb09c290f3b5d001a58
DiegoCP17 <diego.cordoba@correo.tdea.edu.co>
2023-03-30 17:40:30 -0500

    correccion de espacion en home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,26 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  // packages = [
-  //   {
-  //     place: 'Apartamentos J.O',
-  //     location: 'Antioquia',
-  //     bg:'/src/assets/Imagenes/pintor.jpg'
-      
-  //   },
-  //   {
-  //     place: 'Edificio los olivos',
-  //     location: 'Cali',
-  //     bg:'/src/assets/Imagenes/Electricista.jpg'
-      
-  //   },
-  //   {
-  //     place: 'Aguas Claras',
-  //     location: 'Santa Marta',
-  //     bg:'/src/assets/Imagenes/plomero.jpg'
-      
-  //   }
-  // ]
 
   Salir() {
     
